297193bafb88509238daa78d564d72a4b710c2b0
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-10-03 00:08:32 -0400

    Began work on part 3. Server now correctly grabs new client.

--- hw2p3/client.cpp
+++ hw2p3/client.cpp
@@ -1,27 +1,25 @@
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
+#define MESSAGE_LIMIT 1024
 
 int main()
 {
     //  Prepare our context and socket
     zmq::context_t context(1);
-    zmq::socket_t socket(context, zmq::socket_type::req);
+    zmq::socket_t socket(context, zmq::socket_type::dealer);
 
-    std::cout << "Connecting to hello world server..." << std::endl;
     socket.connect("tcp://localhost:5555");
 
-    //  Do 10 requests, waiting each time for a response
-    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq::message_t request(5);
-        memcpy(request.data(), "Hello", 5);
-        std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
-        socket.send(request, zmq::send_flags::none);
-
+    zmq::message_t request;
+    char buffer[] = "Connecting";
+    memcpy(request.data(), buffer, 11);
+    socket.send(request, zmq::send_flags::none);
+    while (true) {
         //  Get the reply.
         zmq::message_t reply;
         socket.recv(reply, zmq::recv_flags::none);
-        std::cout << "Received World " << request_nbr << std::endl;
+        std::cout << reply.data() << std::endl;
     }
     return 0;
 }
--- hw2p3Server/server.cpp
+++ hw2p3Server/server.cpp
@@ -1,6 +1,9 @@
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
+#include <list>
+#define MESSAGE_LIMIT 1024
+
 #ifndef _WIN32
 #include <unistd.h>
 #else
@@ -9,26 +12,47 @@
 #define sleep(n)	Sleep(n)
 #endif
 
+struct Client{
+    int iteration = 1;
+    int id = -1;
+};
+
+
 int main() {
     //  Prepare our context and socket
     zmq::context_t context(2);
-    zmq::socket_t socket(context, zmq::socket_type::rep);
+    zmq::socket_t socket(context, zmq::socket_type::dealer);
     socket.bind("tcp://*:5555");
 
-    while (true) {
-        zmq::message_t request;
-
+    int iterations = 0;
+    int numClients = 0;
+    while (true) { 
+        std::list<Client> clients;
         //  Wait for next request from client
-        socket.recv(request, zmq::recv_flags::none);
-        std::cout << "Received Hello" << std::endl;
-
-        //  Do some 'work'
-        sleep(1);
+        zmq::message_t reply;
+        zmq::recv_result_t received(socket.recv(reply, zmq::recv_flags::dontwait));
+        if ( ( received.has_value() && ( EAGAIN != received.value() ) ) ) {
+            Client newClient;
+            newClient.id = numClients++;
+            clients.push_front(newClient);
+            std::cout << "RAN\n";
+        }
+        char rtnString[MESSAGE_LIMIT] = "";
+        for (Client i : clients) {
+            char clientString[MESSAGE_LIMIT];
+            sprintf_s(clientString, "Client %d: Iteration %d\n", i.id, i.iteration);
+            strcat_s(rtnString, clientString);
+            i.iteration++;
+        }
+        
+        //Slow down for readability
 
         //  Send reply back to client
-        zmq::message_t reply(5);
-        memcpy(reply.data(), "World", 5);
-        socket.send(reply, zmq::send_flags::none);
+        if (numClients > 0) {
+            zmq::message_t message;
+            memcpy(message.data(), rtnString, strlen(rtnString) + 1);
+            socket.send(message, zmq::send_flags::none);
+        }
     }
     return 0;
 }
