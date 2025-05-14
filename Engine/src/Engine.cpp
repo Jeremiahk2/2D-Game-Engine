cf8886ead3b0d9afb80179228d1b6a93cc865a92
Jeremiah Knizley <TheMainMoe@gmail.com>
2025-05-14 19:18:32 -0400

    Converted from sin to CMake. Moved core engine code to new folder structure. Updated gitignore with CMakeUserPresets.

---
+++ Engine/src/Engine.cpp
@@ -0,0 +1,28 @@
+﻿// Engine.cpp : Defines the entry point for the application.
+//
+
+
+#include "Engine.h"
+#include <optional>
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!", sf::Style::None);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
--- Games/Snake/GameClient/scripts/move_character.js
+++
@@ -1,4 +0,0 @@
-//This better be the character
-var obj = moreArgs();
-obj.x = obj.x + obj.speedX
-obj.y = obj.y + obj.speedY
